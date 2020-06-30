716a4de27c982f34c9f01272909d1ae5fcbe04c4
Ron Schrama <r.schrama@gmail.com>
2020-06-30 16:31:47 +0200

    fix for filessytem error

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,13 +39,13 @@ module.exports = {
         name: 'blog',
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/images/`,
-        name: `images`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     path: `${__dirname}/src/images/`,
+    //     name: `images`,
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
